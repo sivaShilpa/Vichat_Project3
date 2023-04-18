38bdbf3a14dd3e305f6d93607108d6f0640d9b47
Jose Aguirre <aguirrejose615@gmail.com>
2023-04-18 11:30:01 -0400

    addong friend model

--- vichat_app/models.py
+++ vichat_app/models.py
@@ -1,3 +1,36 @@
+from http.client import HTTPResponse
+from smtplib import SMTPResponseException
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    friends = models.ManyToManyField("User", blank=True)
+
+class Friend_Request(models.Model):
+    from_user = models.ForeignKey(
+      User, related_name='From_user', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(
+      User, related_name='to_user', on_delete=models.CASCADE)
+
+@login_required
+def send_friend_request(request, userID):
+    from_user = request.user
+    to_user = User.objects.get(id=userID)
+    friend_request, created = Friend_Request.objects.get_or_create(
+      from_user=from_user, to_user=to_user)
+    if created:
+        return HTTPResponse('Friend request sent!')
+    else:
+        return HTTPResponse('Friend request was already sent')
+
+@login_required
+def accept_friend_request(request, requestID):
+    friend_request = Friend_Request.objects.get(id=requestID)
+    if friend_request.to_user == request.user:
+        friend_request.to_user.friends.add(friend_request.from_user)
+        friend_request.from_user.friends.add(friend_request.to_user)
+        friend_request.delete()
+        return HTTPResponse('friend request accepted')
+    else:
+        return HTTPResponse('friend request not accepted')
