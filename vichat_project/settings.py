5adc4ed94fa47e0982f2326b0a4c58324868e66c
AnthonyScott <anthony.scott1005@gmail.com>
2023-04-20 20:01:13 -0400

    Chat histories are created automatically upon users becoming friends

--- vichat_project/settings.py
+++ vichat_project/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from decouple import config
-import environ
-environ.Env() 
-environ.Env.read_env()
+# import environ
+# environ.Env() 
+# environ.Env.read_env()
 SECRET_KEY = config('SECRET_KEY')
-print(config('AWS_ACCESS_KEY_ID'))
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
